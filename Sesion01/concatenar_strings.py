22963f2616aee702ca84a9c4078e6eedbff0c4e2
xguntrr <josephotto2005@gmail.com>
2026-08-14 02:16:58 -0500

    Sesion01: formateo de strings

---
+++ Sesion01/concatenar_strings.py
@@ -0,0 +1,22 @@
+persona_nombre = "Olmer"
+persona_edad = 15
+
+#Forma simple
+print("El nombre de la persona:", persona_nombre)
+print("La edad de la persona:", persona_edad)
+
+# f: formatear
+print(f"La edad de la persona: {persona_edad}. Y el nombre de la persona es: {persona_nombre}")
+
+a = 12
+b = 13
+
+suma = a + b
+
+# Codigo dentro del formato
+print(f"La suma de a + b: {a + b}")
+print(f"La suma de a + b: {suma}")
+
+
+# Salto de linea
+print(f"La edad de la persona: {persona_edad}.\nEl nombre de la persona es: {persona_nombre}")
